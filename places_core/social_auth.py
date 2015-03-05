31ba146a9049a22a38f9173329e8791cf7b29db8
Jakub Pocentek <jpocentek@gmail.com>
2015-03-05 13:09:32 +0100

    Formularz do wysyłania wiadomości

--- places_core/social_auth.py
+++ places_core/social_auth.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 from social.pipeline.partial import partial
 from social.exceptions import AuthException
 from userspace.models import UserProfile
-from userspace.helpers import random_string
+from userspace.helpers import random_string, create_username
 
 
 def obtain_user_social_profile(response):
@@ -125,9 +125,10 @@ def create_user_profile(strategy, details, response, user=None, *args, **kwargs)
         except Token.DoesNotExist:
             token = Token.objects.create(user=user)
             token.save()
-        try: profile = UserProfile.objects.get(user=user)
+        try:
+            profile = UserProfile.objects.get(user=user)
         except UserProfile.DoesNotExist:
-            profile = UserProfile(user = user)
+            profile = UserProfile(user=user)
             if 'gender' in response:
                 profile.gender = obtain_user_gender(response)
             profile.save()
@@ -139,6 +140,8 @@ def get_username(strategy, details, user=None, *args, **kwargs):
     if user:
         final_username = storage.user.get_username(user)
     else:
+        import json
+        print json.dumps(details)
         final_username = random_string(30)
         while storage.user.user_exists(username=final_username):
             final_username = random_string(30)
