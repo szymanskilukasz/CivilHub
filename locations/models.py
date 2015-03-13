1bda45794eae77562e35b3bc9e1851eefba7eb33
Jakub Pocentek <jpocentek@gmail.com>
2015-03-13 15:39:30 +0100

    Skompresowane skrypty

--- locations/models.py
+++ locations/models.py
@@ -311,13 +311,12 @@ class Location(models.Model, BackgroundModelMixin):
         return sorted(qs, key=lambda x: x['date_created'], reverse=True)
 
     def __str__(self):
-        # lang = get_language().split('-')[0]
-        # alt = self.names.filter(language=lang)
-        # if not len(alt):
-        #     return self.name
-        # else:
-        #     return alt[0].altername
-        return self.name
+        lang = get_language().split('-')[0]
+        alt = self.names.filter(language=lang)
+        if not len(alt):
+            return self.name
+        else:
+            return alt[0].altername
 
 
 @python_2_unicode_compatible
