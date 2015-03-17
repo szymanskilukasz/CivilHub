ccd2f9b3993ad7229a1b9fb2c6b713fa0e73a54a
Jakub Pocentek <jpocentek@gmail.com>
2015-03-17 15:46:45 +0100

    Poprawione skrypty dla komentarzy pod Chrome

--- places_core/forms.py
+++ places_core/forms.py
@@ -2,6 +2,9 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
+
+from haystack.forms import ModelSearchForm
+
 from .models import AbuseReport
 
 
@@ -38,3 +41,8 @@ class AbuseReportForm(forms.ModelForm):
     class Meta:
         model = AbuseReport
         fields = ('content_type', 'object_pk', 'comment',)
+
+
+class CivilSearchForm(ModelSearchForm):
+    """ """
+    pass
