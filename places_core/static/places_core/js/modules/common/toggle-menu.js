596d69fb382eccdcb802a8afababc2e0fe1b0987
Jakub Pocentek <jpocentek@gmail.com>
2015-03-20 15:38:40 +0100

    Poprawione ładowanie skryptów

---
+++ places_core/static/places_core/js/modules/common/toggle-menu.js
@@ -0,0 +1,59 @@
+//
+// toggle-menu.js
+// ==============
+
+// Otwiera menu z opcjami w widokach szczegółowych
+// różnych typów zawartości po kliknięciu w przycisk.
+
+require(['jquery'], function ($) {
+
+"use strict";
+
+function SubMenu ($el) {
+	this.$el = $el;
+	this.$submenu = this.$el.parent().next('.entry-submenu');
+	this.is_menu_opened = false;
+	this.initialize();
+}
+
+SubMenu.prototype.initialize = function () {
+	var element = this.$el;
+  this.$el.on('click', function (e) {
+  	e.preventDefault();
+  	SubMenu.prototype.toggle.call(this);
+  }.bind(this));
+};
+
+SubMenu.prototype.open = function () {
+	this.$submenu.slideDown('fast', function () {
+		$('body').one('click', function () {
+			this.close();
+		}.bind(this));
+		this.is_menu_opened = true;
+	}.bind(this));
+};
+
+SubMenu.prototype.close = function () {
+	this.$submenu.slideUp('fast', function () {
+		this.is_menu_opened = false;
+	}.bind(this));
+};
+
+SubMenu.prototype.toggle = function () {
+	if (this.is_menu_opened)
+		this.close();
+	else
+		this.open();
+};
+
+$.fn.simpleSubMenu = function () {
+	return $(this).each(function () {
+		$(this).data('submenu', new SubMenu($(this)));
+	});
+};
+
+$(document).ready(function () {
+	$('.submenu-toggle').simpleSubMenu();
+});
+
+});
